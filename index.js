78c192b2cfbd32f63c9ce0538cae154b8b16bccb
readandwrite864 <63188034+readandwrite864@users.noreply.github.com>
2024-04-29 20:22:59 +0700

    add: markup

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
+import { data } from "./data.js";
+
+function Category({ name, docs }) {
+  const self = this;
+  self.name = name;
+  self.docs = ko.observableArray(docs.map((doc) => new Doc(doc)));
+}
+
+function Doc({ name }) {
+  const self = this;
+  self.name = name;
+}
+
 function AppViewModel() {
-  this.message = ko.observable("Hello World!");
+  const self = this;
+  self.categories = ko.observableArray(
+    data.map((category) => new Category(category))
+  );
 }
 
 ko.applyBindings(new AppViewModel());
